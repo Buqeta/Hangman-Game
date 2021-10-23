05d3ea8e7c3317c820731921ebfa55550f448198
Buqeta <73654883+Buqeta@users.noreply.github.com>
2021-10-23 12:27:03 +0200

    Add files via upload

---
+++ hangman_project.py
@@ -0,0 +1,130 @@
+import random
+
+# Functions of the game
+
+def mistakes(counter):
+    """ prints out the hangman according to the number of mistakes made
+        up to a specific point in the game """
+    if counter == 1:
+        print('  ------\n |     |\n |\n |\n |\n |\n_|_')
+    elif counter == 2:
+        print('  ------\n |     |\n |     o\n |\n |\n |\n_|_')
+    elif counter == 3:
+        print('  ------\n |     |\n |     o\n |     |\n |     |\n |\n_|_')
+    elif counter == 4:
+        print('  ------\n |     |\n |     o\n |    /|\\\n |     |\n |\n_|_')
+    elif counter == 5:
+        print('  ------\n |     |\n |     o\n |    /|\\\n |     |\n |    / \\\n_|_')
+    else:
+        print('Counter should be between one and five!')
+
+
+
+def print_progress(list):
+    """ prints out the word with the guessed letters up to now """
+    for i in range(len(list)):
+        print(list[i], end = ' ')
+
+def insert_letters(list ,indexes, letter):
+    """ inserts the letter/s in the correct indices of the list
+        that contains the rightly guessed letters """
+    for i in indexes:
+        list[i] = letter
+
+def find_letters_in_word(word, letter):
+    """ finds the indices at which a letter appears in a word,
+        returns a list with those indices """
+    t = list()
+    index = -5
+    for i in range(len(word)):
+        if word.find(letter, i) != index and word.find(letter, i) != -1:
+            index = word.find(letter, i)
+            t.append(index)
+    if bool(t) == False:
+        return False
+    return t
+
+def completed(list1, list2):
+    """ checks if the guessed word is the same as the word that should be guessed """
+    for i in range(len(list1)):
+        if list1[i] != list2[i]:
+            return False
+    return True
+
+
+
+
+
+# The start of the game     
+
+print("Welcome to the Hangman Game!")
+print("Type 'exit' to exit the game!")
+
+fin = open('random_words.txt')
+words = fin.read().split('\n')
+word = random.choice(words) #the word the user should guess
+print('\n\nGuess the word with ', len(word), ' letters!')
+word_list = list(word)
+guessed_letters = list(len(word) * '_')
+print_progress(guessed_letters)
+count_mistakes = 0
+all_guesses = list()
+
+while True:
+    guess = input('\n\nGuess a letter: ')
+    if not guess.isalpha():
+        print('The guess should be a letter! Guess again!')
+        continue
+    elif guess in all_guesses:
+        print('You already guessed this letter.')
+        continue
+    elif guess == 'exit':
+        break
+    else:  
+        indices = find_letters_in_word(word, guess)
+        all_guesses.append(guess)
+        if indices == False:    #if the letter wasn't found in the word
+            count_mistakes += 1
+            mistakes(count_mistakes)
+            if count_mistakes >= 5:
+                print('\nYou failed.')
+                guess = input('Press Enter to play again or type "exit" to exit the program: ')
+                if guess == 'exit':
+                    break
+                if guess == '':
+                    word = random.choice(words)
+                    print('\n\nGuess the word with ', len(word), ' letters!')
+                    word_list = list(word)
+                    guessed_letters = list(len(word) * '_')
+                    print_progress(guessed_letters)
+                    count_mistakes = 0
+                    all_guesses = list()
+                    
+        else:   # if the letter was found in the word
+            insert_letters(guessed_letters ,indices, guess)
+            if completed(guessed_letters, word_list) == True:
+                print('You won!')
+                print_progress(guessed_letters)
+                guess = input('\n\nPress Enter to play again or type "exit" to exit the program: ')
+                if guess == 'exit':
+                    break
+                if guess == '':
+                    word = random.choice(words)
+                    print('Guess the word with ', len(word), ' letters!')
+                    word_list = list(word)
+                    guessed_letters = list(len(word) * '_')
+                    count_mistakes = 0
+                    all_guesses = list()
+            print_progress(guessed_letters)
+            
+    
+        
+
+
+
+
+
+
+
+
+
